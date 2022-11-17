46b51293601c21215daf4090cc8e8a4b21b4d07e
rann00b <110574373+rann00b@users.noreply.github.com>
2022-11-17 07:05:33 +0000

    Use custom log

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/api/callback', (req, res) => {
+    console.log(JSON.stringify({
+        params: req.params,
+        headers: req.headers,
+        body: req.body,
+    }, null, 2))
+
     res.end('Callback received.')
 })
 
