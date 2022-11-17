fe747f31744fd9b9f5db86fd7edb31096777d69c
rann00b <110574373+rann00b@users.noreply.github.com>
2022-11-17 07:04:08 +0000

    Remove loggers

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const logging = require('express-logging');
-const logger = require('logops');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(logging(logger))
 app.use('/api/callback', (req, res) => {
     res.end('Callback received.')
 })
