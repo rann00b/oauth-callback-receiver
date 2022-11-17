366ebf0765eeed329c2f27cd075c45a85507c7dd
rann00b <110574373+rann00b@users.noreply.github.com>
2022-11-17 07:14:11 +0000

    Remove the /api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/api/callback', (req, res) => {
+app.use('/callback', (req, res) => {
     console.log(JSON.stringify({
         params: req.query,
         headers: req.headers,
