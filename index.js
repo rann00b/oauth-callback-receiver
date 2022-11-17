7f474438046b57a7d4ab6b37c55a172f9e93e166
rann00b <110574373+rann00b@users.noreply.github.com>
2022-11-17 06:47:05 +0000

    Add logger

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(morgan('dev'))
 app.use('/api/callback', (req, res) => {
     res.end('Callback received.')
 })
