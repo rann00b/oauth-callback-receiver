5f8309df50c66d400840587eb566a1d9308a3fec
rann00b <110574373+rann00b@users.noreply.github.com>
2022-11-21 00:54:14 +0000

    Clean up code

--- src/routes/gitlab/index.js
+++ src/routes/gitlab/index.js
@@ -1,5 +1,4 @@
 import got from 'got';
-import pkce from 'pkce-challenge';
 import { nanoid } from 'nanoid';
 import qs from 'qs';
 
@@ -11,7 +10,6 @@ export function gitlabRoute(app) {
   const redirectUri = 'https://oauth-callback-receiver.vercel.app/gitlab/callback';
 
   app.use('/gitlab/authorize', (req, res) => {
-    const challenge = pkce.default();
     const state = nanoid();
     const query = qs.stringify({
       client_id: process.env.GITLAB_CLIENT_ID,
@@ -19,12 +17,9 @@ export function gitlabRoute(app) {
       response_type: 'code',
       state,
       scope: 'api read_api read_user read_repository write_repository',
-      // code_challenge_method: 'S256',
-      // code_challenge: challenge.code_challenge,
     });
 
     states.set(state, true);
-    // states[state] = { code_verifier: challenge.code_verifier };
 
     res.redirect('https://gitlab.com/oauth/authorize?' + query);
   });
@@ -46,12 +41,9 @@ export function gitlabRoute(app) {
             code,
             grant_type: 'authorization_code',
             redirect_uri: redirectUri,
-            // code_verifier: states[state].code_verifier,
           },
         })
         .json();
-
-      console.log(response);
 
       res.type('html');
       res.send(`
@@ -59,7 +51,6 @@ export function gitlabRoute(app) {
         <script>window.opener.postMessage('${JSON.stringify(response)}', '*')</script>`);
     } catch (error) {
       console.log(error);
-      console.log(`${error}`);
       res.end('Failed to get your access token.');
     }
   });
