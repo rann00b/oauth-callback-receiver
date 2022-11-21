4fa20ccfa208cf3ab8f0f3536014bbb560307f71
rann00b <110574373+rann00b@users.noreply.github.com>
2022-11-21 00:20:04 +0000

    Add content-type

--- src/routes/gitlab/index.js
+++ src/routes/gitlab/index.js
@@ -40,6 +40,7 @@ export function gitlabRoute(app) {
     try {
       const response = await got
         .post('https://gitlab.com/oauth/token', {
+          headers: { 'Content-Type': 'application/json' },
           body: {
             client_id: process.env.GITLAB_CLIENT_ID,
             code,
